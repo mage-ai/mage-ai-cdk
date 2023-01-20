d47bca71c21877f82bb1fe31b7fb68529c342693
wangxiaoyou1993 <xiaoyou@mage.ai>
2023-01-19 18:00:18 -0800

    [xy] Deploy Mage app to ECS cluster.

---
+++ typescript/lib/constants.ts
@@ -0,0 +1,16 @@
+// Application Environment
+export const APP_ENVIRONMENT = 'production';
+// Application Name
+export const APP_NAME = 'mage-data-prep-cdk';
+// List of availability zones
+export const AVAILABILITY_ZONES = ['us-west-2a', 'us-west-2b'];
+// The CIDR block for the VPC
+export const CIDR = '10.32.0.0/16';
+// Docker image url used in ECS task
+export const DOCKER_IMAGE = 'mageai/mageai:latest';
+// ECS task cpu
+export const ECS_TASK_CPU = 512;
+// ECS task memory
+export const ECS_TASK_MEMORY = 1024;
+// List of public subnets
+export const PUBLIC_SUBNETS = ['10.32.100.0/24', '10.32.101.0/24'];
--- typescript/lib/mage-ai-stack.ts
+++ typescript/lib/mage-ai-stack.ts
@@ -1,5 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import {
+  APP_ENVIRONMENT,
+  APP_NAME,
+  AVAILABILITY_ZONES,
+  CIDR,
+  DOCKER_IMAGE,
+  ECS_TASK_CPU,
+  ECS_TASK_MEMORY,
+} from './constants'
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as ecs from "aws-cdk-lib/aws-ecs";
 import * as ecs_patterns from "aws-cdk-lib/aws-ecs-patterns";
@@ -11,26 +20,31 @@ export class MageAiStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'MageAiCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-    const vpc = new ec2.Vpc(this, "MyVpc", {
-      maxAzs: 3 // Default is all AZs in region
+    const vpc = new ec2.Vpc(this, `${APP_NAME}-vpc`, {
+      availabilityZones: AVAILABILITY_ZONES,
+      cidr: CIDR,
     });
 
-    const cluster = new ecs.Cluster(this, "MyCluster", {
+    const cluster = new ecs.Cluster(this, `${APP_NAME}-${APP_ENVIRONMENT}-cluster`, {
       vpc: vpc
     });
 
     // Create a load-balanced Fargate service and make it public
-    new ecs_patterns.ApplicationLoadBalancedFargateService(this, "MyFargateService", {
-      cluster: cluster, // Required
-      cpu: 512, // Default is 256
-      desiredCount: 6, // Default is 1
-      taskImageOptions: { image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample") },
-      memoryLimitMiB: 2048, // Default is 512
-      publicLoadBalancer: true // Default is true
+    const taskDefinition = new ecs.FargateTaskDefinition(this, `${APP_NAME}-task`, {
+      cpu: ECS_TASK_CPU,
+      memoryLimitMiB: ECS_TASK_MEMORY,
+    });
+
+    taskDefinition.addContainer('Config', {
+      image: ecs.ContainerImage.fromRegistry(DOCKER_IMAGE),
+      portMappings: [{containerPort : 6789, hostPort: 6789}],
+    });
+
+    new ecs_patterns.ApplicationLoadBalancedFargateService(this, `${APP_NAME}-${APP_ENVIRONMENT}-ecs-service`, {
+      cluster: cluster,
+      desiredCount: 1,
+      taskDefinition:taskDefinition,
+      publicLoadBalancer: true,
     });
   }
 }
